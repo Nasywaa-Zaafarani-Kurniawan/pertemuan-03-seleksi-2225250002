91c7038a04f5068cadbdc1357cb9ccc6a03117aa
Nasywaa Zaafarani Kurniawan <nasywaakurniawan@gmail.com>
2026-09-17 18:48:00 +0700

    commit file-file latihan

---
+++ latihan/01_genap_ganjil.py
@@ -0,0 +1,6 @@
+bilangan = int(input("masukan bilangan bulat: "))
+
+if bilangan % 2 == 0:
+    print (f"{bilangan} adalah bilangan genap")
+else:
+    print (f"{bilangan} adalah bilangan ganjil")   
